4a05cb65e33a1070bea3331b3d058093102a068f
minsu kim <45655623+kmsu44@users.noreply.github.com>
2023-03-01 01:03:18 +0900

    [Gold V] Title: LCS, Time: 316 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,14 +1,28 @@
-str1 = input()
-str2 = input()
-m = len(str1)+1
-n = len(str2)+1
-Memo = [[0 for _ in range(m)] for _ in range(n)]
-for i in range(n):
-    for j in range(m):
-        if i == 0 or j == 0:
-            continue
-        elif str1[j-1] == str2[i-1]:
-            Memo[i][j] = Memo[i-1][j-1] + 1
-        else:
-            Memo[i][j] = max(Memo[i-1][j], Memo[i][j-1])
-print(Memo[-1][-1])
+import sys
+from sys import stdin
+
+input = stdin.readline
+
+s1 = input().strip()
+s2 = input().strip()
+
+
+def get_lcs(s1: str, s2: str) -> int:
+    prev = [0] * (len(s2) + 1)
+    curr = [0] * (len(s2) + 1)
+
+    for i in range(1, len(s1) + 1):
+        for j in range(1, len(s2) + 1):
+            if s1[i-1] == s2[j-1]:
+                curr[j] = prev[j-1] + 1
+            else:
+                curr[j] = max(curr[j-1], prev[j])
+
+        prev, curr = curr, prev
+
+    return prev[len(s2)]
+
+
+print(get_lcs(s1, s2))
+
+
