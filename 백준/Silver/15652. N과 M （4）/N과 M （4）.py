96c04d53f70cda09e0ca1de9141fcdb108b4a439
Minsu Kim <45655623+kmsu44@users.noreply.github.com>
2023-03-13 11:12:23 +0900

    [Silver III] Title: N과 M (4), Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+
+
+def BT(depth, route):
+    if depth == m:
+        print(*route)
+        return
+    for i in range(route[-1], n+1):
+        route.append(i)
+        BT(depth+1, route)
+        route.pop()
+
+
+for i in range(1, n+1):
+    BT(1, [i])
