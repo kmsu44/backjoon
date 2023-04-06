cece6756b393432b60a3c93a025d03a209f7d9d5
Minsu Kim <45655623+kmsu44@users.noreply.github.com>
2023-04-06 16:39:53 +0900

    [Gold II] Title: K번째 수, Time: 176 ms, Memory: 114488 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.py
+++ 백준/Gold/1300. K번째 수/K번째 수.py
@@ -1,16 +1,18 @@
-from collections import defaultdict
-n = int(input())
-k = int(input())
-left = 1
-right = min(10**9, n * n)
-while left <= right:
-    mid = (left + right)//2
-    cnt = 0
-    for i in range(1, n+1):
-        cnt += min(mid // i, n)
-    if cnt >= k:
-        right = mid - 1
-    else:
-        left = mid + 1
+import sys
 
-print(left)
+N = int(sys.stdin.readline())
+k = int(sys.stdin.readline())
+
+s, e = 1, N**2
+while s <= e:
+    mid = (s + e) // 2
+    cnt = 0
+
+    for i in range(1, N + 1):
+        cnt += min(mid // i, N)
+
+    if cnt >= k:
+        e = mid - 1
+    else:
+        s = mid + 1
+print(s)
