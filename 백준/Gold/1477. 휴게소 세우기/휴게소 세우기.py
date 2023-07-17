ee2857ab611a67d50cf445de0c3cd9c03f078398
Minsu Kim <45655623+kmsu44@users.noreply.github.com>
2023-07-17 14:17:22 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.py
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.py
@@ -11,7 +11,7 @@ pos.sort()
 l, r = 1, L-1
 
 ans = 0
-while l <= r:
+while l < r:
     cnt = 0
     mid = (l+r) // 2
     for i in range(1, len(pos)):
@@ -20,7 +20,7 @@ while l <= r:
     if cnt > M:
         l = mid+1
     else:
-        r = mid-1
-        ans = mid
+        r = mid
 
-print(ans)
+
+print(r)
