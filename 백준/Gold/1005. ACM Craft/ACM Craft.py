81c8c439d4a9b1e76a61fcb09db029a2a5c48fb2
Minsu Kim <45655623+kmsu44@users.noreply.github.com>
2023-07-26 23:36:05 +0900

    [Gold III] Title: ACM Craft, Time: 520 ms, Memory: 125960 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.py
+++ 백준/Gold/1005. ACM Craft/ACM Craft.py
@@ -1,62 +1,24 @@
-# 1
-# 5 10
-# 100000 99999 99997 99994 99990
-# 4 5
-# 3 5
-# 3 4
-# 2 5
-# 2 4
-# 2 3
-# 1 5
-# 1 4
-# 1 3
-# 1 2
-# 4
+from sys import stdin, setrecursionlimit
+input = stdin.readline
 
-# 9시 10q분 시작
-from collections import defaultdict, deque
-import sys
+def cal_delay(now: int) -> int:
+    if not tree[now] : return delay[now]
+    if total_delay[now] != -1: return total_delay[now]
 
-T = int(input())
-result = []
+    for build in tree[now]:
+        total_delay[now] = max(total_delay[now], cal_delay(build))
+    total_delay[now] += delay[now]
+    return total_delay[now]
 
+t = int(input())
+for _ in range(t):
+    n, k = [int(x) for x in input().split()]
+    delay = [0] + [int(x) for x in input().split()]
+    tree = [[] for _ in range(n+1)]
+    total_delay = [-1] * (n+1)
+    for _ in range(k):
+        s, e = [int(x) for x in input().split()]
+        tree[e].append(s)
+    goal = int(input())
 
-def findcost(W):
-    if dp[W] != -1:
-        return dp[W]
-    for i in delay_reverse[W]:
-        dp[W] = max(dp[W], findcost(i) + rule[W-1])
-    if dp[W] == -1:
-        dp[W] = rule[W-1]
-    return dp[W]
-
-
-for _ in range(T):
-    N, K = map(int, input().split())
-    rule = list(map(int, input().split()))
-    delay_reverse = defaultdict(list)
-    for _ in range(K):
-        a, b = map(int, input().split())
-        delay_reverse[b].append(a)
-
-    start_list = []
-    for i in range(1, N+1):
-        if delay_reverse[i]:
-            continue
-        else:
-            start_list.append(i)
-
-    W = int(input())
-    dp = [-1 for _ in range(N+1)]
-    findcost(W)
-    result.append(dp[W])
-for i in result:
-    print(i)
-
-# 1
-# 4 3
-# 1 1 1 1
-# 1 2
-# 3 2
-# 1 4
-# 4
+    print(cal_delay(goal))
