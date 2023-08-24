aedd7bf0416ca5fdbcc28cd828928acdb8fd763d
Minsu <kmsu444@gmail.com>
2023-08-24 22:18:23 +0900

    [level 3] Title: 양과 늑대, Time: 9.61 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.py
@@ -1,29 +1,57 @@
-def dfs(idx, sheep, wolf, possible):
-    global g_info, answer, graph
-    if g_info[idx] == 0:
-        sheep += 1
-        answer = max(answer, sheep)
-    else:
-        wolf += 1
+from collections import deque
+import copy
+
+
+def BFS(sheep, wolf, info, tree):
+    global possible
+    q = deque()
+    q.append((0, copy.deepcopy(sheep), copy.deepcopy(wolf)))
+    while q:
+        node, tmpsheep, tmpwolf = q.popleft()
+        # print(node, tmpsheep, tmpwolf)
         
-    if wolf >= sheep:
-        return 
-    
-    possible.extend(graph[idx])
-    
-    for p in possible:
-        dfs(p, sheep, wolf, [i for i in possible if i != p])
-    
+        if len(tmpsheep) == len(tmpwolf):
+            continue
+        if len(tmpsheep) > len(sheep):
+            possible.append([tmpsheep, tmpwolf])
+        for child in tree[node]:
+            if info[child] == 0:
+                if child in tmpsheep:
+                    q.append((child, copy.deepcopy(
+                        tmpsheep), copy.deepcopy(tmpwolf)))
+                else:
+                    tmpsheep.add(child)
+                    q.append((child, copy.deepcopy(
+                        tmpsheep), copy.deepcopy(tmpwolf)))
+                    tmpsheep.remove(child)
+
+            else:
+                if child in tmpwolf:
+                    q.append((child, copy.deepcopy(
+                        tmpsheep), copy.deepcopy(tmpwolf)))
+                else:
+                    tmpwolf.add(child)
+                    q.append((child, copy.deepcopy(
+                        tmpsheep), copy.deepcopy(tmpwolf)))
+                    tmpwolf.remove(child)
+
+
 def solution(info, edges):
-    global answer, g_info, visited, graph
-    answer = 0
-    g_info = info
-    n = len(info)
-    graph = [[] for _ in range(n)]
-    
-    for a, b in edges:
-        graph[a].append(b)
-        
-    
-    dfs(0, 0, 0, [])
+    global possible
+    tree = [[] for _ in range(len(info))]
+    for parent, child in edges:
+        tree[parent].append(child)
+    sheep = set([0])
+    wolf = set()
+    answer = len(sheep)
+    c = 0
+    possible = [[sheep, wolf]]
+    answer = len(sheep)
+    while possible:
+        # print('sheep', sheep, wolf)
+        sheep, wolf = possible.pop()
+        answer = max(answer, len(sheep))
+        BFS(sheep, wolf, info, tree)
+
     return answer
+
