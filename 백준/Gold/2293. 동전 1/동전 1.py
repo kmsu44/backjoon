7f5298d5c1c9ba573557b74245b4510037be4b9c
MINSU KIM <kmsu444@gmail.com>
2024-07-18 16:26:57 +0900

    [Gold V] Title: 동전 1, Time: 136 ms, Memory: 111512 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -0,0 +1,12 @@
+n, k = map(int,input().split())
+
+coins = [int(input()) for _ in range(n)]
+
+dp = [0 for _ in range(k+1)]
+dp[0] = 1
+
+for coin in coins:
+    for price in range(coin,k+1):
+        dp[price] += dp[price-coin]
+
+print(dp[-1])     
